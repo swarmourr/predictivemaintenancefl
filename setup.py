3c7eb68a6109cebdabe3384c9b2086dc1efca6f1
swarmourr <safrihamza.ensasafi@gmail.com>
2023-02-19 21:16:27 +0100

    ADD PROJECT TREE

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='In this project we will  builde federated learning application for Predictive maintenance using the NASA/IMS,CWRU and FEMTO datasets',
+    author='Hamza safri',
+    license='',
+)
